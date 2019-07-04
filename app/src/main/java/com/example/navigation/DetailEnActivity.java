cd3428bc878ea8193b271873aba247b1e80a478a
TUUED <tunguyen.ued@gmail.com>
2019-07-04 14:16:31 +0700

    ud

--- app/src/main/java/com/example/navigation/DetailEnActivity.java
+++ app/src/main/java/com/example/navigation/DetailEnActivity.java
@@ -130,4 +130,11 @@ public class DetailEnActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(DetailEnActivity.this, ListEngineerActivity.class);
+        startActivity(intent);
+        super.onBackPressed();
+    }
 }
